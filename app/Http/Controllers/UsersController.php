8735ec561dd98de8d02f011d37b2256077225acb
switch <switch_zhou@163.com>
2020-04-11 07:33:36 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('users.create');
     }
+
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    //Model name为User单数形式，默认指定为users数据表，当想指定别的数据表的时候，可以用以下的方法
     protected $table = 'users';
 
 
@@ -39,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
